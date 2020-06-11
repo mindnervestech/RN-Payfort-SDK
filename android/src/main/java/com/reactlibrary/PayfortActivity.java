0ece379ba865bd2f610e48ea847b13ad6f6ff8fe
Kartikaiya Mayatwal <kartike30@gmail.com>
2020-06-11 14:03:38 +0530

    Android Payfort Upgrade

--- android/src/main/java/com/reactlibrary/PayfortActivity.java
+++ android/src/main/java/com/reactlibrary/PayfortActivity.java
@@ -1,13 +1,10 @@
 package com.reactlibrary;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.util.Log;
-import android.view.View;
-import android.widget.ProgressBar;
+import android.app.ProgressDialog;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.NetworkResponse;
@@ -19,12 +16,22 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.JsonRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.WritableNativeArray;
+import com.facebook.react.bridge.WritableNativeMap;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.payfort.fort.android.sdk.base.FortSdk;
 import com.payfort.fort.android.sdk.base.callbacks.FortCallBackManager;
 import com.payfort.fort.android.sdk.base.callbacks.FortCallback;
 import com.payfort.sdk.android.dependancies.base.FortInterfaces;
 import com.payfort.sdk.android.dependancies.models.FortRequest;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -32,6 +39,7 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class PayfortActivity extends Activity {
@@ -39,7 +47,8 @@ public class PayfortActivity extends Activity {
     FortCallBackManager fortCallback;
     ProgressDialog pbLoading;
     String deviceId, isLive, accessCode, merchantIdentifier, requestPhrase, language,
-            customerEmail, currency, amount, merchantReference, customerName, customerIp, paymentOption, orderDescription;
+            customerEmail, currency, amount, merchantReference, customerName, customerIp, paymentOption, orderDescription,
+            responsePhrase, sdkToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,40 +65,44 @@ public class PayfortActivity extends Activity {
         deviceId = FortSdk.getDeviceId(PayfortActivity.this);
 
         requestGetToken();
+        //startFortRequest(sdkToken);
     }
 
     private void parseData() {
         Intent intent = getIntent();
         if (intent.hasExtra("is_live")) {
             isLive = intent.getStringExtra("is_live");
-        }
+        } //
         if (intent.hasExtra("access_code")) {
             accessCode = intent.getStringExtra("access_code");
-        }
+        } //
         if (intent.hasExtra("merchant_identify")) {
             merchantIdentifier = intent.getStringExtra("merchant_identify");
-        }
+        } //
         if (intent.hasExtra("request_phrase")) {
             requestPhrase = intent.getStringExtra("request_phrase");
+        } //
+        if (intent.hasExtra("response_phrase")) {
+            responsePhrase = intent.getStringExtra("response_phrase");
         }
         if (intent.hasExtra("customer_email")) {
             customerEmail = intent.getStringExtra("customer_email");
-        }
+        } //
         if (intent.hasExtra("language")) {
             language = intent.getStringExtra("language");
-        }
+        } //
         if (intent.hasExtra("currency")) {
             currency = intent.getStringExtra("currency");
-        }
+        } //
         if (intent.hasExtra("amount")) {
             amount = intent.getStringExtra("amount");
-        }
+        } //
         if (intent.hasExtra("merchant_reference")) {
             merchantReference = intent.getStringExtra("merchant_reference");
-        }
+        } //
         if (intent.hasExtra("customer_name")) {
             customerName = intent.getStringExtra("customer_name");
-        }
+        } //
         if (intent.hasExtra("customer_ip")) {
             customerIp = intent.getStringExtra("customer_ip");
         }
@@ -98,7 +111,10 @@ public class PayfortActivity extends Activity {
         }
         if (intent.hasExtra("order_description")) {
             orderDescription = intent.getStringExtra("order_description");
-        }
+        } //
+        // if (intent.hasExtra("sdk_token")) {
+        //     sdkToken = intent.getStringExtra("sdk_token");
+        // }
     }
 
     private void requestGetToken() {
@@ -170,7 +186,7 @@ public class PayfortActivity extends Activity {
         fortrequest.setShowResponsePage(true);
 
         String environment = FortSdk.ENVIRONMENT.TEST;
-        if(isLive.equals("1")) {
+        if (isLive.equals("1")) {
             environment = FortSdk.ENVIRONMENT.PRODUCTION;
         }
 
@@ -182,7 +198,7 @@ public class PayfortActivity extends Activity {
                     Log.e("Cancelled ", responseMap.toString());
                     finish();
                     String message = "Payment has been canceled";
-                    if(responseMap.get("response_message") != null && !responseMap.get("response_message").toString().equals("")) {
+                    if (responseMap.get("response_message") != null && !responseMap.get("response_message").toString().equals("")) {
                         message = responseMap.get("response_message").toString();
                     }
                     RNPayfortSdkModule.onFail.invoke(message);
@@ -193,7 +209,15 @@ public class PayfortActivity extends Activity {
                         Object> fortResponseMap) {
                     Log.e("Success ", fortResponseMap.toString());
                     finish();
-                    RNPayfortSdkModule.onSuccess.invoke(fortResponseMap.toString());
+
+                    JSONObject responseObj = new JSONObject(fortResponseMap);
+                    try {
+                        WritableMap productMap = convertJsonToMap(responseObj);
+
+                        RNPayfortSdkModule.onSuccess.invoke(productMap);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                 }
 
                 @Override
@@ -202,16 +226,75 @@ public class PayfortActivity extends Activity {
                     Log.e("Failure ", fortResponseMap.toString());
                     finish();
                     String message = "There is an error while process payment";
-                    if(fortResponseMap.get("response_message") != null && !fortResponseMap.get("response_message").toString().equals("")) {
+                    if (fortResponseMap.get("response_message") != null && !fortResponseMap.get("response_message").toString().equals("")) {
                         message = fortResponseMap.get("response_message").toString();
                     }
                     RNPayfortSdkModule.onFail.invoke(message);
+                    // JSONObject responseObj = new JSONObject(fortResponseMap);
+                    // try {
+                    //     WritableMap productMap = convertJsonToMap(responseObj);
+
+                    //     RNPayfortSdkModule.onSuccess.invoke(productMap);
+                    // } catch (JSONException e) {
+                    //     e.printStackTrace();
+                    // }
                 }
 
             });
         } catch (Exception e) {
             Log.e("execute Payment", "call FortSdk", e);
         }
+    }
+
+    private static WritableMap convertJsonToMap(JSONObject jsonObject) throws JSONException {
+        WritableMap map = new WritableNativeMap();
+
+        Iterator<String> iterator = jsonObject.keys();
+        while (iterator.hasNext()) {
+            String key = iterator.next();
+            Object value = jsonObject.get(key);
+            if (value instanceof JSONObject) {
+                map.putMap(key, convertJsonToMap((JSONObject) value));
+            } else if (value instanceof JSONArray) {
+                map.putArray(key, convertJsonToArray((JSONArray) value));
+            } else if (value instanceof Boolean) {
+                map.putBoolean(key, (Boolean) value);
+            } else if (value instanceof Integer) {
+                map.putInt(key, (Integer) value);
+            } else if (value instanceof Double) {
+                map.putDouble(key, (Double) value);
+            } else if (value instanceof String) {
+                map.putString(key, (String) value);
+            } else {
+                map.putString(key, value.toString());
+            }
+        }
+        //map.putString("device_id", FortSdk.getDeviceId(PayfortActivity.this));
+        return map;
+    }
+
+    private static WritableArray convertJsonToArray(JSONArray jsonArray) throws JSONException {
+        WritableArray array = new WritableNativeArray();
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            Object value = jsonArray.get(i);
+            if (value instanceof JSONObject) {
+                array.pushMap(convertJsonToMap((JSONObject) value));
+            } else if (value instanceof JSONArray) {
+                array.pushArray(convertJsonToArray((JSONArray) value));
+            } else if (value instanceof Boolean) {
+                array.pushBoolean((Boolean) value);
+            } else if (value instanceof Integer) {
+                array.pushInt((Integer) value);
+            } else if (value instanceof Double) {
+                array.pushDouble((Double) value);
+            } else if (value instanceof String) {
+                array.pushString((String) value);
+            } else {
+                array.pushString(value.toString());
+            }
+        }
+        return array;
     }
 
     private Map<String, Object> collectRequestMap(String sdkToken) {
@@ -223,11 +306,18 @@ public class PayfortActivity extends Activity {
         requestMap.put("language", language);
         requestMap.put("merchant_reference", merchantReference);
         requestMap.put("customer_name", customerName);
-        requestMap.put("customer_ip", customerIp);
-        requestMap.put("payment_option", paymentOption);
+        if (customerIp != null) {
+            requestMap.put("customer_ip", customerIp);
+        }
+        if (paymentOption != null) {
+            requestMap.put("payment_option", paymentOption);
+        }
         requestMap.put("eci", "ECOMMERCE");
-        requestMap.put("order_description", orderDescription);
+        if (orderDescription != null) {
+            requestMap.put("order_description", orderDescription);
+        }
         requestMap.put("sdk_token", sdkToken);
+        Log.e("PayfortMap", requestMap.toString());
         return requestMap;
     }
 
